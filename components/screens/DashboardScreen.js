1ce7aa242734820857ca09fe0f5d23f47f068126
leepuppychow <leepuppychow@gmail.com>
2018-04-10 17:18:03 -0600

    Renders component for today's workout on DashboardScreen

--- components/screens/DashboardScreen.js
+++ components/screens/DashboardScreen.js
@@ -10,15 +10,45 @@ import {
   AsyncStorage,
 } from 'react-native';
 import Header from '../Header'
+import WorkoutsContainer from '../workouts/WorkoutsContainer'
 
 type Props = {};
 export default class DashboardScreen extends Component<Props> {
+
+  constructor() {
+    super()
+    this.state = {
+      workouts: [],
+    }
+  }
+
+  componentDidMount() {
+    this.getTodaysWorkouts()
+  }
+
+  getToken = () => {
+    return AsyncStorage.getItem('jwt').then(token => JSON.parse(token)["jwt"])
+  }
+
+  getTodaysWorkouts = () => {
+    this.getToken()
+      .then(token => {
+        fetch("https://my-hep.herokuapp.com/api/v1/workouts/today",
+          {
+            "headers": {"Authorization": "Bearer " + token}
+          })
+          .then(response => response.json())
+          .then(workouts => this.setState({ workouts }))
+          .catch(error => console.warn({ error }))
+      })
+  }
 
   render() {
     return (
       <View style={ styles.container }>
         <Header header="Dashboard"/>
         <Text>Workout for the day</Text>
+        <WorkoutsContainer workouts={ this.state.workouts } />
         <Text>Your Activity Levels</Text>
       </View>
     );
