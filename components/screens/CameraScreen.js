fd77bc03495e6a68752913e0f3578af13f757308
leepuppychow <leepuppychow@gmail.com>
2018-04-10 15:52:14 -0600

    Connects to phone and is able to load images to Amazon S3 bucket; API keys protected

--- components/screens/CameraScreen.js
+++ components/screens/CameraScreen.js
@@ -11,6 +11,8 @@ import {
 } from 'react-native';
 import Header from '../Header'
 import { Camera, Permissions } from 'expo'
+import { RNS3 } from 'react-native-aws3'
+import config from '../../config'
 
 type Props = {};
 export default class CameraScreen extends Component<Props> {
@@ -28,12 +30,50 @@ export default class CameraScreen extends Component<Props> {
     this.setState( { hasCameraPermission: status === 'granted' })
   }
 
+  ohhSnap = () => {
+    this.camera.takePictureAsync()
+      .then(data => {
+        const file = {
+          uri: data.uri,
+          name: 'photo_YAHHH.jpg',
+          type: 'image/jpeg'
+        }
+
+        const bucketOptions = {
+          keyPrefix: 'photos/',
+          bucket: 'my-hep-images',
+          region: 'us-east-2',
+          accessKey: config['ACCESS_KEY'],
+          secretKey: config['SECRET_KEY'],
+          successActionStatus: 201,
+        }
+
+        RNS3.put(file, bucketOptions)
+          .then(response => {
+            if(response.status === 201) {
+              AlertIOS.alert("Photo uploaded successfully")
+            } else {
+              console.warn(`Failed to upload image: ${response.body}`)
+            }
+          })
+      })
+      .catch(error => console.warn(`ERROR: ${error}`))
+  }
+
   render() {
     return (
       <View style={ styles.container }>
         <Header header="Take picture of handout"/>
-        <Camera style={ {flex: 1} } type={ this.state.type }>
+        <Camera
+          ref={ cam => this.camera = cam }
+          style={ {flex: 1} }
+          type={ this.state.type }>
         </Camera>
+        <Button
+          title='TAKE PICTURE'
+          style={ styles.snapButton }
+          onPress={ this.ohhSnap }
+        />
       </View>
     )
   }
@@ -46,4 +86,8 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
   },
+  snapButton: {
+    fontSize: 20,
+    textAlign: 'center',
+  }
 });
